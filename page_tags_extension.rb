00b0ded521789443eba5d54b8ce6173fcf940477
Victor Bogado <victor@bogado.net>
2009-03-16 21:19:58 -0300

    initial extension

---
+++ page_tags_extension.rb
@@ -0,0 +1,23 @@
+# Uncomment this if you reference any of your controllers in activate
+# require_dependency 'application'
+
+class PageTagsExtension < Radiant::Extension
+  version "1.0"
+  description "Describe your extension here"
+  url "http://yourwebsite.com/page_tags"
+  
+  # define_routes do |map|
+  #   map.namespace :admin, :member => { :remove => :get } do |admin|
+  #     admin.resources :page_tags
+  #   end
+  # end
+  
+  def activate
+    # admin.tabs.add "Page Tags", "/admin/page_tags", :after => "Layouts", :visibility => [:all]
+  end
+  
+  def deactivate
+    # admin.tabs.remove "Page Tags"
+  end
+  
+end
